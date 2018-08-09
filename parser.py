fd8be7a62d3a72bd05d6eac58db1229fe78e8811
Nick Terskikh <sir.pc-help@yandex.ru>
2018-08-09 23:35:06 +0800

    Версия 0.1 Выводит список всех сайтов на экране.

--- parser.py
+++ parser.py
@@ -2,8 +2,25 @@ __author__ = 'Nick Terskikh'
 # -*- coding: utf-8 -*-
 # 09.08.2018
 
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    response = requests.get(url)
+    return response.text
+
+
+def get_data_items(html):
+    soup = BeautifulSoup(html, 'lxml')
+    items = soup.find_all('a', {'class' : 'link link_outer_yes link_theme_outer path__item i-bem'})
+
+    return [a.get('href') for a in items]
+
+
 def main():
-    pass
+    url = 'https://yandex.ru/search/?clid=9582&text=скачать&lr=118890&p=1'
+    print(get_data_items(get_html(url)))
 
 
 if __name__ == '__main__':
