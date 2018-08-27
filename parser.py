7da756051a3cc3dda13c50e828a51d6e8c543e1b
Nick Terskikh <sir.pc-help@yandex.ru>
2018-08-27 13:20:24 +0800

    Форматируем вывод

--- parser.py
+++ parser.py
@@ -2,9 +2,9 @@ __author__ = 'Nick Terskikh'
 # -*- coding: utf-8 -*-
 # 09.08.2018
 
+import csv
 import requests
 from bs4 import BeautifulSoup
-import csv
 
 
 def get_html(url):
@@ -14,7 +14,8 @@ def get_html(url):
 
 def get_data_items(html):
     soup = BeautifulSoup(html, 'lxml')
-    items = soup.find_all('a', {'class' : 'link link_outer_yes link_theme_outer path__item i-bem'})
+    items = soup.find_all(
+        'a', {'class': 'link link_outer_yes link_theme_outer path__item i-bem'})
     return [a.get('href') for a in items]
 
 
@@ -22,7 +23,9 @@ def main():
     url = 'https://yandex.ru/search/?clid=9582&text=скачать&lr=118890&p=1'
     print('Парсим следующий url:\n')
     print(url)
-    print('Результат: \n', get_data_items(get_html(url)))
+    # print('\nРезультат: \n\n', get_data_items(get_html(url)))
+    print('\nРезультат: \n\n', get_data_items(
+        get_html(url).replace('https://', 'pass').replace('http://', 'pass')))
 
 
 if __name__ == '__main__':
